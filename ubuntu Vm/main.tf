6fbde5e9a05fe06a2737fe1413954d66c636f6fa
Anu Aniyammal S <anuaniyammal@gmail.com>
2024-11-16 16:07:52 +0530

    Update main.tf

--- ubuntu Vm/main.tf
+++ ubuntu Vm/main.tf
@@ -1,52 +1,13 @@
-# Define the provider
-provider "azurerm" {
-  features {}
-}
-
-# Create a resource group
-resource "azurerm_resource_group" "example" {
-  name     = "example-resource-group"
-  location = "East US"
-}
-
-# Create a virtual network
-resource "azurerm_virtual_network" "example" {
-  name                = "example-vnet"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-}
-
-# Create a subnet
-resource "azurerm_subnet" "example" {
-  name                 = "example-subnet"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
-  address_prefixes     = ["10.0.1.0/24"]
-}
-
-# Create a network interface
-resource "azurerm_network_interface" "example" {
-  name                = "example-nic"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-
-  ip_configuration {
-    name                          = "example-ip-config"
-    subnet_id                     = azurerm_subnet.example.id
-    private_ip_address_allocation = "Dynamic"
-  }
-}
-
-# Create a public IP address
+# Public IP resource
 resource "azurerm_public_ip" "example" {
   name                = "example-public-ip"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
-  allocation_method   = "Dynamic"
+  allocation_method   = "Static" # Required for Standard SKU
+  sku                 = "Standard"
 }
 
-# Create the virtual machine
+# Virtual Machine with correct image reference
 resource "azurerm_linux_virtual_machine" "example" {
   name                = "example-vm"
   resource_group_name = azurerm_resource_group.example.name
@@ -58,7 +19,7 @@ resource "azurerm_linux_virtual_machine" "example" {
   ]
   admin_ssh_key {
     username   = "adminuser"
-    public_key = file("~/.ssh/id_rsa.pub") # Path to your SSH public key
+    public_key = file("~/.ssh/id_rsa.pub")
   }
 
   os_disk {
@@ -67,15 +28,4 @@ resource "azurerm_linux_virtual_machine" "example" {
   }
 
   source_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "20_04-lts"
-    version   = "latest"
-  }
-}
-
-# Output the VM's public IP address
-output "public_ip_address" {
-  value = azurerm_public_ip.example.ip_address
-}
-
+    publisher = "Canonic
